ae567cd9321fb66f76f4a2508c76f9560504cd36
haden9 <haden9@gmail.com>
2016-01-30 18:03:51 -0600

    Complete rq02

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,10 @@
 class Place
 
+  def self.load_all(json_file)
+    json_hash = JSON.parse(json_file.read)
+    collection.insert_many(json_hash)
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
