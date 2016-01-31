e4b4d8a670901a8262e9dc12bbc6913975209d9d
haden9 <haden9@gmail.com>
2016-01-30 19:46:34 -0600

    Complete query rq02

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,7 +5,7 @@ class Place
   def initialize(params={})
     @id = params[:_id].to_s
     @formatted_address = params[:formatted_address]
-    @location = Point.new(params[:location]) if params[:location].present?
+    @location = Point.new(params[:geometry][:location]) if params[:geometry].present?
     @address_components = []
     params[:address_components].each do |address_component|
       @address_components << AddressComponent.new(address_component)
@@ -14,6 +14,14 @@ class Place
 
   def self.find_by_short_name(short_name)
     collection.find({'address_components.short_name' => short_name})
+  end
+
+  def self.to_places(collection_view)
+    places = []
+    collection_view.each do |view|
+      places << Place.new(view)
+    end
+    return places
   end
 
   def self.load_all(json_file)
