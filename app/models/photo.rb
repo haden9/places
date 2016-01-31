464e7835e4593d8017680c512d90be527cc612be
haden9 <haden9@gmail.com>
2016-01-31 03:29:34 -0600

    Complete photo rq04

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,38 @@
 class Photo
-  attr_accessor :id, :location, :contents
+  attr_accessor :id, :location
+  attr_writer :contents
+
+  include ActiveModel::Model
 
   def initialize(params={})
-    @id = params[:id].present? ? params[:id] : params[:_id].to_s
+    if params[:_id]
+      @id = params[:_id].to_s
+    else
+      @id = params[:id]
+    end
     @location = Point.new(params[:metadata][:location]) if params[:metadata].present?
+  end
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def save
+    unless persisted?
+      if @contents
+        description = {}
+        description[:content_type] = {}
+        description[:metadata] = {}
+        description[:metadata][:location] = {}
+        gps = EXIFR::JPEG.new(@contents).gps
+        @location = Point.new({lng: gps.longitude, lat: gps.latitude})
+        description[:content_type] = 'image/jpeg'
+        description[:metadata][:location] = @location.to_hash
+        grid_file = Mongo::Grid::File.new(@contents.read, description)
+        id = self.class.mongo_client.database.fs.insert_one(grid_file)
+        @id = id.to_s
+      end
+    end
   end
 
   def self.mongo_client
