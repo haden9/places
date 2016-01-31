ca5d77e74b0527d897301e9830f7fc4edea4b8bc
haden9 <haden9@gmail.com>
2016-01-30 19:05:44 -0600

    Complete rq05

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,17 @@
 class Place
 
+  attr_accessor :id, :formatted_address, :location, :address_components
+
+  def initialize(params={})
+    @id = params[:_id].to_s
+    @formatted_address = params[:formatted_address]
+    @location = Point.new(params[:location]) if params[:location].present?
+    @address_components = []
+    params[:address_components].each do |address_component|
+      @address_components << AddressComponent.new(address_component)
+    end
+  end
+
   def self.load_all(json_file)
     json_hash = JSON.parse(json_file.read)
     collection.insert_many(json_hash)
