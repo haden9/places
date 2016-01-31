76e195c48873c1de1c12e0c0496e12992deaa662
haden9 <haden9@gmail.com>
2016-01-31 02:18:35 -0600

    Complete photo rq03

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,9 @@
 class Photo
   attr_accessor :id, :location, :contents
 
-  def initialize
+  def initialize(params={})
+    @id = params[:id].present? ? params[:id] : params[:_id].to_s
+    @location = Point.new(params[:metadata][:location]) if params[:metadata].present?
   end
 
   def self.mongo_client
